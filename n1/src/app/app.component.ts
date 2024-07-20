9f8dab26bdbf8c6a59f621224e157e1ffd6a56ef
MuratDzh <murat_dzhumatov@mail.ru>
2024-07-20 14:59:19 +0300

    n1-2

--- n1/src/app/app.component.ts
+++ n1/src/app/app.component.ts
@@ -10,5 +10,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'n1';
+  counter=0;
+  updatedAt?:number;
+
+  get onDisabled():boolean{
+    return !this.counter
+  }
+
+  increase(){
+    this.counter++;
+    this.updatedAt=Date.now();
+  }
+
+  decrease(){
+    this.counter--;
+    this.updatedAt=Date.now();
+  }
+
+  clear(){
+    this.counter=0;
+    this.updatedAt=Date.now();
+  }
 }
